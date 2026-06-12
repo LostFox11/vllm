2f8540b684c9b3891b0a1fdc61a1ab2f6501d2e8
LostFox11 <wangziyue17@huawei.com>
2026-06-12 10:11:25 +0800

    pp+1

--- vllm/v1/executor/ray_executor.py
+++ vllm/v1/executor/ray_executor.py
@@ -102,7 +102,7 @@ class RayDistributedExecutor(Executor):
         meaning that it allows PP size batches to be executed concurrently.
         """
         pp_size = self.parallel_config.pipeline_parallel_size
-        return 2 if pp_size <= 1 and self.scheduler_config.async_scheduling else pp_size
+        return 2 if pp_size <= 1 and self.scheduler_config.async_scheduling else pp_size + 1
 
     def shutdown(self) -> None:
         if logger:
